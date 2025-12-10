e419b222f3000b901e35963695f318c11c37d405
Muhammad <mf9555759@gmail.com>
2025-12-10 15:50:19 +0800

    hapus produk versi hapus foto di folder jga

--- controllers/deleteProduct.php
+++ controllers/deleteProduct.php
@@ -9,6 +9,30 @@ include '../config/koneksi.php';
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
+
+     // Ambil data produk
+    $get = mysqli_query($conn, "SELECT image FROM products WHERE id='$id' LIMIT 1");
+    $data = mysqli_fetch_assoc($get);
+
+    if ($data) {
+
+        $publicPath = $data['image'];
+
+        // Hapus path projek-uas/ utk dapat path sebenarnya
+        $relativePath = str_replace("/projek-uas", "..", $publicPath);
+
+        $serverFile = $relativePath;
+
+        //Hapus filenya
+        if (file_exists($serverFile)) {
+            unlink($serverFile);
+        }
+    }
+
+
+
+    //hapus di db
+
     $query = "DELETE FROM products WHERE id = '$id'";
 
     if (mysqli_query($conn, $query)) {
